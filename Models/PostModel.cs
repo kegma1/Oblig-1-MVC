b65a234cce8d61adeaaa6ada6095656e43cac3e2
Kennet Mathisen <kennetam02@gmail.com>
2024-09-23 17:09:44 +0200

    it may worketh?

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Post
 {
     public int Id { get; set; } 
-    public string Title { get; set; } = "";
-    public string Content { get; set; } = "";
+
+    [Required]
+    [MaxLength(50)]
+    public string Title { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+    
+    [Required]
     public DateTime CreatedAt { get; set; } 
     public User? Author { get; set; }
 }
